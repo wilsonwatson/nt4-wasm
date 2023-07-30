a6fd11b1f12c480281bf471b544ff874fd8e95fd
wilsonwatson <wilsonwatson@utexas.edu>
2023-07-30 14:29:39 -0500

    watchValue helper

--- src/next_part.js
+++ src/next_part.js
@@ -3,3 +3,17 @@ async function run(name, ip) {
     await wasm_bindgen('./nt4_wasm_bg.wasm');
     await _nt4_start(name, ip);
 }
+
+function watchValue(id, f) {
+    const target = document.getElementById(id);
+    const config = { attributes: true, childList: false, subtree: false, attributeFilter: ["value"] };
+    const callback = (mutationList, _) => {
+        for (const mutation of mutationList) {
+            if (mutation.type === "attributes") {
+                f(JSON.parse(mutation.target.getAttribute(mutation.attributeName)));
+            }
+        }
+    }
+    const observer = new MutationObserver(callback);
+    observer.observe(target, config);
+}
